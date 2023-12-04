977cac0979a456419a2bcea702c71974f3227d7e
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-12-04 16:31:45 +0000

    solve task 6

---
+++ bin/a6.rs
@@ -0,0 +1,14 @@
+// challange create a rust counter program
+
+
+
+fn main(){
+    let mut counter = 5;
+
+    while counter >= 1 {
+        println!("{:?}",counter);
+
+        counter -= 1;
+
+    }
+}
