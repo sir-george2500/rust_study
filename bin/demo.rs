649e716851b4d92ce57f9f118b1d715dfd2ac67e
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-12-04 16:03:01 +0000

    added the demo rust

---
+++ bin/demo.rs
@@ -0,0 +1,22 @@
+
+
+
+
+fn main(){
+
+    let mut i = 3;
+
+    loop {
+        println!("{:?}", i);
+        i = i - 1;
+        if i == 0 {
+            break;
+        }
+
+
+    }
+    println!("done!");
+
+
+
+}
