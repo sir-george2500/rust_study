a20ae9278b5ba08b7d2abc0ba3658cec1c4441ea
George S. Mulbah <gsmulbah2500@gmail.com>
2023-12-29 16:53:19 +0000

    added the enum

---
+++ src/bin/a4.rs
@@ -0,0 +1,16 @@
+
+
+
+
+
+fn main(){
+    let my_number = 8;
+
+    match my_number {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("other")
+
+    }
+}
---
+++ src/bin/demo_enum.rs
@@ -0,0 +1,17 @@
+
+enum Direction {
+    Left,
+    Right,
+}
+
+fn main(){
+    let go = Direction::Left;
+
+    match go {
+        Direction::Left => println!("go left"),
+        Direction::Right => println!("go right"),
+    }
+
+
+
+}
